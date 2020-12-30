bef8550c6b77732aab06805b6338803e1fd0f7d2
RyanClinton777 <58789023+RyanClinton777@users.noreply.github.com>
2020-12-30 20:20:50 +0000

    Set up for components, routing, bootstrap. Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'; //Import react Component class
+import 'bootstrap/dist/css/bootstrap.min.css'; //react-bootstrap stylesheet
+import {Navbar, Nav }from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; //Router for navigation
+//Component imports
+import { Home } from './components/home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      //NAVIGATION BAR FROM REACT BOOTSTRAP
+      <Router>
+        <div className="App">
+          {/* Navbar */}
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+          </Navbar>
+          {/* Routing */}
+          <Switch>
+            <Route path='/' component={Home} exact ></Route>
+          </Switch>
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//Add export keyword so we can import the class
+export class Home extends React.Component {
+    render() {
+      return (
+        <h1>Home page</h1>
+      );
+    }
+  }
